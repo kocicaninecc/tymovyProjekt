cf7726360c0c6cd069a64408980b1dcd6cca770b
Dokoupil <dokoupil@netoffice.cz>
2022-01-31 10:47:27 +0100

    trida Dokoupil

---
+++ Dokoupil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tymovyProjekt
+{
+    class Dokoupil
+    {
+        public string gay { get; set; }
+        public string jes { get; set; }
+    }
+}
